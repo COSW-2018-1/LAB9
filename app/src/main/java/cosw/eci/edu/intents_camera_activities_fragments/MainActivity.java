b47e63a206c0969fe6be5ea2fca0ca3ab2270745
Sergio Perez <dark07perez@gmail.com>
2018-04-21 23:21:54 -0500

    requisitos de calificacion 1, Terminados

--- app/src/main/java/cosw/eci/edu/intents_camera_activities_fragments/MainActivity.java
+++ app/src/main/java/cosw/eci/edu/intents_camera_activities_fragments/MainActivity.java
@@ -8,11 +8,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,29 +71,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveAll(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Desea salir de la aplicación?");
-        builder.setTitle("Salir de App");
 
 
-        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
+        if(text.getText().toString().equals("enter message") || text.getText().toString().equals("")){
+            Toast toast1 = Toast.makeText(getApplicationContext(), "Please enter either a message or select an image ", Toast.LENGTH_LONG);
+            toast1.setGravity(Gravity.CENTER,0 ,0 );
+            toast1.show();
+
+        }
+        else{
+
+           if(text.getText().toString().length() > 50){
+               Toast toast1 = Toast.makeText(getApplicationContext(), "The message is very long, maximum 50 characters ", Toast.LENGTH_LONG);
+               toast1.setGravity(Gravity.CENTER,0 ,0 );
+               toast1.show();
+           }
+           else{
+
+               AlertDialog.Builder builder = new AlertDialog.Builder(this);
+               builder.setMessage("Desea salir de la aplicación?");
+               builder.setTitle("Salir de App");
 
 
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
+               builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       finish();
+                   }
+               });
 
 
-        AlertDialog dialog = builder.create();
-        dialog.show();
+               builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       dialog.cancel();
+                   }
+               });
+
+
+               AlertDialog dialog = builder.create();
+               dialog.show();
+
+           }
+
+
+        }
+
+
 
     }
 
