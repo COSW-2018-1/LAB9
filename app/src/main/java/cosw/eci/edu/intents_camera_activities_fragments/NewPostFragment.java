cada1f3360e3ce30f05e25a2caa1587a23516ae3
Sergio Perez <dark07perez@gmail.com>
2018-05-03 05:35:57 -0500

    con botón de fragment

---
+++ app/src/main/java/cosw/eci/edu/intents_camera_activities_fragments/NewPostFragment.java
@@ -0,0 +1,40 @@
+package cosw.eci.edu.intents_camera_activities_fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ *
+ */
+public class NewPostFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+
+    public void onClickLoadPhoto(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),10);
+    }
+
+}
